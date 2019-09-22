45b305c871421a0dbf68d09578fd7dc3c887b51c
Joaquim Barreto <joaquim@barreto.co>
2019-09-22 22:05:17 +0200

    Done

--- Vehicles/Program.cs
+++ Vehicles/Program.cs
@@ -6,7 +6,30 @@ namespace Vehicles
     {
         static void doWork()
         {
-            // TODO:
+            Console.WriteLine("Journey by airplane:");
+            Airplane myPlane = new Airplane();
+            myPlane.StartEngine("Contact");
+            myPlane.TakeOff();
+            myPlane.Drive();
+            myPlane.Landing();
+            myPlane.StopEngine("Whirr");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Journey by car:");
+            Car myCar = new Car();
+            myCar.StartEngine("Brumm Brmm");
+            myCar.Accelerate();
+            myCar.Drive();
+            myCar.Brake();
+            myCar.StopEngine("Phut Phut");
+
+            Console.WriteLine("\nTesting polymorphism");
+            Vehicle v = myCar;
+            v.Drive();
+            v = myPlane;
+            v.Drive();
+
         }
 
         static void Main()
